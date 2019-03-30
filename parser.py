6eb0efb863e7d95c10f4c018616cd93ad180ef60
Yaroslav <yaroslav01@ukr.net>
2019-03-30 12:07:18 +0200

    working on a parser

--- parser.py
+++ parser.py
@@ -1,15 +1,27 @@
 import sys
+import csv
+
+MVA_TABLE_PATH = "mva_table.csv"
 
 DEF_KWD = 0
 END_KWD = 1
 START_KWD = 2
 RETURN_KWD = 3
+OUT_KWD = 4
+IN_KWD = 5
+CONSOLE_KWD = 6
+CALL_KWD = 7
 
-def parse(string: [str]):
+def read_rules(path: str):
+    f = open(path)
+    reader = csv.reader(f.readlines())
+    f.close()
+
+def parse(strings: [str], rule: dict = read_rules(MVA_TABLE_PATH)):
     token = []
-    pass
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-        file = open(sys.argv[1])
-        parse(file.readlines)
+        f = open(sys.argv[1])
+        parse(f.readlines)
+        f.close()
